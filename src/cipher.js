6c5f50ce057fe03e4a915ece9cdd4148205ad83c
LMVR90 <luzmarcelavasquez@gmail.com>
2019-06-11 18:59:05 -0500

    Esta versión activa los botones de la pantalla de bienvenida para ir a pantalla de cifrar y pantalla de descifrar. Activa botón de regresar a pantalla de inicio.

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,12 @@
+/*Implementar el objeto cipher, el cual debe estar exportado en el objeto global (window). (sólo hay que introducir funciones al método).
+Este objeto (cipher) debe contener dos métodos:
+cipher.encode(offset, string):
+offset es el número de posiciones que queremos mover a la derecha en el alfabeto y string el mensaje (texto) que queremos cifrar.
+cipher.decode(offset, string):
+offset es el número de posiciones que queremos mover a la izquierda en el alfabeto y string el mensaje (texto) que queremos descifrar*/
+
+/*Esto es un objeto*/
+
 window.cipher = {
-  // ... 
+  // ...
 };
--- src/index.js
+++ src/index.js
@@ -1,20 +1,51 @@
+/*COMPRUEBA QUE EL DOM SE CARGUE COMPLETAMENTE ANTES DE EJECUTAR CÓDIGO*/
 document.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM fully loaded and parsed');
 });
 
+/*DECLARA VARIABLES DEL DOM*/
 const userMessage = document.getElementById('user-message');
 const offset = document.getElementById('offset');
+const buttonCode = document.getElementById('button-code');
+const buttonDecode = document.getElementById('button-decode');
+const buttonStartAgain = document.getElementById('start-again-button');
 
-const buttonCipher = document.getElementById('button-cipher');
-const buttonDecipher = document.getElementById('button-decipher');
 
+/*FUNCIÓN PARA REGRESAR A PANTALLA DE BIENVENIDA*/
+const startAgain = () => {
+  document.getElementById('welcome').style.display="block";
+  document.getElementById('code-screen').style.display="none";
+  document.getElementById('decode-screen').style.display="none";
+  document.getElementById('start-again').style.display="none";
+  return codeScreen;
+}
+
+/*FUNCIÓN PARA IR A PANTALLA DE TEXTO CODIFICADO*/
+const codeScreen = () => {
+  document.getElementById('welcome').style.display="none";
+  document.getElementById('code-screen').style.display="block";
+  document.getElementById('start-again').style.display="block";
+  return codeScreen;
+}
+
+/*FUNCIÓN PARA IR A PANTALLA DE TEXTO DECODIFICADO*/
+const decodeScreen = () => {
+  document.getElementById('welcome').style.display="none";
+  document.getElementById('decode-screen').style.display="block";
+  document.getElementById('start-again').style.display="block";
+  return decodeScreen;
+}
+
+/*DECLARA BOTONES Y EVENTOS DEL DOM*/
+buttonCode.addEventListener("click", codeScreen);
+buttonDecode.addEventListener("click", decodeScreen);
+buttonStartAgain.addEventListener("click",startAgain);
+
+/* COMPROBAR SI GUARDA RESPUESTAS
 const saveAnswers = () => {
-/*userMessage = document.getElementById('user-message').value;
-offset = document.getElementById('offset').value;
-=*/
 console.log(userMessage.value);
 console.log(offset.value);
 document.addEventListener('click',saveAnswers)
 }
-
 saveAnswers();
+*/
